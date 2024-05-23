46aca94ee5e48a736f312e9f69336307f5046374
johnsalemOT <john@octathorn.com>
2024-05-23 23:10:24 +0500

    Responsive Hamburger Menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import "./navbar.scss";
-import logo from "../../../public/logo.png";
+import logo from "/logo.png";
+import menu from "/menu.png";
 
 function Navbar() {
+    const [open, setOpen] = useState(false);
+    
   return (
     <nav>
       <div className="left">
@@ -20,7 +24,20 @@ function Navbar() {
       </div>
       <div className="right">
         <a href="/">Sign In</a>
-        <a href="/" className="register">Sign Up</a>
+        <a href="/" className="register">
+          Sign Up
+        </a>
+        <div className={open ? "menuIcon active": "menuIcon"}>
+          <img src={menu} alt="mobile menu" onClick={() => setOpen(prev => !prev)}></img>
+        </div>
+      <div className={open ? "menu active" : "menu"}>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+        <a href="/">Sign in</a>
+        <a href="/">Sign up</a>
+      </div>
       </div>
     </nav>
   );
